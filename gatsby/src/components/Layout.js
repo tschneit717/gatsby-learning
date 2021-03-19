0e7ce3cf8a26916c12c19f8753981f5da5598e0f
tschneit717 <tschneit717@gmail.com>
2021-03-19 16:05:53 -0400

    Module 7-10: Set up basic styles

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,13 +1,46 @@
 import React from 'react';
+import styled from 'styled-components';
+import 'normalize.css';
+
 import Footer from './Footer';
+import GlobalStyles from '../styles/GlobalStyles';
 import Nav from './Nav';
+import Typography from '../styles/Typography';
+import stripes from '../assets/images/stripes.svg';
+
+const SiteBorderStyles = styled.div`
+  max-width: 1000px;
+  margin: 12rem auto 4rem auto;
+  margin-top: clamp(2rem, 10vw, 12rem);
+  background: white url(${stripes});
+  background-size: 80em;
+  border: 5px solid white;
+  padding: 5px;
+  padding: clamp(5px, 1vw, 25px);
+  box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.05);
+  @media (max-width: 1100px) {
+    margin-left: 1.5rem;
+    margin-right: 1.5rem;
+  }
+`;
+
+const ContentStyles = styled.div`
+  background: white;
+  padding: 2rem;
+`;
 
 export default function Layout({ children }) {
   return (
-    <div>
-      <Nav />
-      {children}
-      <Footer />
-    </div>
+    <>
+      <GlobalStyles />
+      <Typography />
+      <SiteBorderStyles>
+        <ContentStyles>
+          <Nav />
+          {children}
+          <Footer />
+        </ContentStyles>
+      </SiteBorderStyles>
+    </>
   );
 }
