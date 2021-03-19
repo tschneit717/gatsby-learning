cc271f18a7b0375c80129eee7eab046b290de51d
tschneit717 <tschneit717@gmail.com>
2021-03-19 10:18:29 -0400

    Initial commit

---
+++ gatsby/src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function FourOhFourPage() {
+  return (
+    <div>
+      <p>You ain't from around here pardner!</p>
+    </div>
+  );
+}
---
+++ gatsby/src/pages/order.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function OrderPage() {
+  return (
+    <div>
+      <p>What are ya buying?</p>
+    </div>
+  );
+}
